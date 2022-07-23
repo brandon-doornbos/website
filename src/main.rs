582ee18786b2afe42d5258c42d61580d8d8ec10d
Brandon Doornbos <b.doornbos@protonmail.com>
2022-07-23 04:17:06 +0200

    Add an SQLite database and a way to retrieve analytics
    Next step is to insert statistics on visit.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,48 @@
-use rocket::fs::{FileServer, relative};
+use rocket::{
+    fs::{relative, FileServer},
+    get, routes,
+    serde::json::Json,
+};
+use rocket_db_pools::{
+    sqlx::{self, Row, SqlitePool},
+    Connection, Database,
+};
+use serde::Serialize;
+
+#[derive(Database)]
+#[database("analytics")]
+struct Analytics(SqlitePool);
+
+#[derive(Serialize)]
+struct Visit {
+    ip: String,
+    time: String,
+}
+
+#[get("/analytics")]
+async fn analytics(mut db: Connection<Analytics>) -> Json<Vec<Visit>> {
+    Json(
+        sqlx::query(
+            "SELECT ip, strftime('%s', time) as time
+            FROM visits
+            INNER JOIN visitors ON visitors.id = visitor_id;",
+        )
+        .fetch_all(&mut *db)
+        .await
+        .unwrap_or_default()
+        .into_iter()
+        .map(|r| Visit {
+            ip: r.get(0),
+            time: r.get(1),
+        })
+        .collect(),
+    )
+}
 
 #[rocket::launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(Analytics::init())
         .mount("/", FileServer::from(relative!("public")))
+        .mount("/", routes![analytics])
 }
