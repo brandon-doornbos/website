6b9bd77b9013979aceeaceb5f728c259c0b9ce5d
Brandon Doornbos <b.doornbos@protonmail.com>
2022-07-24 04:01:43 +0200

    Add a way to insert visitors and visits into the db

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,73 @@ use rocket_db_pools::{
     Connection, Database,
 };
 use serde::Serialize;
+use std::net::IpAddr;
 
 #[derive(Database)]
 #[database("analytics")]
 struct Analytics(SqlitePool);
+
+#[get("/visit")]
+async fn visit(mut db: Connection<Analytics>, ip: IpAddr) {
+    println!("New visit: {}", ip);
+
+    match get_visitor_id(&mut db, ip).await {
+        None => {
+            match sqlx::query(
+                "INSERT
+                INTO visitors(ip)
+                VALUES (?);",
+            )
+            .bind(ip.to_string())
+            .execute(&mut *db)
+            .await
+            {
+                Ok(_) => {
+                    let id = get_visitor_id(&mut db, ip).await.unwrap_or_default();
+                    insert_visit(&mut db, id).await
+                }
+                Err(err) => println!("Error inserting new visitor: {}", err),
+            }
+        }
+        Some(id) => insert_visit(&mut db, id).await,
+    }
+}
+
+async fn get_visitor_id(db: &mut Connection<Analytics>, ip: IpAddr) -> Option<i64> {
+    match sqlx::query(
+        "SELECT *
+            FROM visitors
+            WHERE ip = ?
+            LIMIT 0,1;",
+    )
+    .bind(ip.to_string())
+    .fetch_one(&mut **db)
+    .await
+    {
+        Ok(row) => Some(row.try_get::<i64, usize>(0).unwrap_or_default()),
+        Err(err) => {
+            println!(
+                "Error getting visitor id (may be on purpose if visitor does not yet exist): {}",
+                err
+            );
+            None
+        }
+    }
+}
+
+async fn insert_visit(db: &mut Connection<Analytics>, id: i64) {
+    if let Err(err) = sqlx::query(
+        "INSERT
+            INTO visits(visitor_id)
+            VALUES (?);",
+    )
+    .bind(id)
+    .execute(&mut **db)
+    .await
+    {
+        println!("Error inserting new visit: {}", err);
+    }
+}
 
 #[derive(Serialize)]
 struct Visit {
@@ -44,5 +107,5 @@ fn rocket() -> _ {
     rocket::build()
         .attach(Analytics::init())
         .mount("/", FileServer::from(relative!("public")))
-        .mount("/", routes![analytics])
+        .mount("/", routes![visit, analytics])
 }
